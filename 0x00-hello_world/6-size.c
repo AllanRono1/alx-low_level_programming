4eb7adba441c11887a81616e0cfb7c0f42bb7e79
Allan Rono <allanrono18@gmail.com>
2023-03-16 17:09:49 -0700

    Task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("Size of a char: %lu.\n", sizeof(char));
+	print("Size of int: %lu.\n", sizeof(int));
+	print("Size of double: %lu.\n", sizeof(double));
+
+	return (0);
+}
