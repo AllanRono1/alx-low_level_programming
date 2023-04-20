992273487dbd7c488f8bd7f72cabbd3fafc5efc5
Allan Rono <allanrono18@gmail.com>
2023-04-20 12:25:55 -0700

    Task 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -9,7 +9,7 @@
  * @...: variable number of numbers to be printed
  */
 
-void print_strings(const char *separator, const unsigned int n, ...)
+void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	unsigned int index;
 	va_list nums;
