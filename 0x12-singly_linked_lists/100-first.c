c76a6a0fd0835bd59c359a512d36e2926725ccc1
Allan Rono <allanrono18@gmail.com>
2023-04-27 07:53:40 -0700

    Task 6

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,7 +3,7 @@
 void first(void) __attribute__ ((constructor));
 
 /**
- * first - function that prints a sentence before the main function is
+ * first - function that prints a string before the main function is
  * executed
  */
 
